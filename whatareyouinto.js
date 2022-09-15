0c55568470f19ced1548ff1d246af6fe38aabdaa
johneyaugusteami <jrmartinez1@my.waketech.edu>
2022-09-15 12:45:49 -0400

    changed afterend to afterbegin

--- whatareyouinto.js
+++ whatareyouinto.js
@@ -19,22 +19,22 @@ function submitAnswers() {
 
     if ((iAM === iMasc && imINTO === theyFem) || (iAM === iFem && imINTO === theyMasc)) {
         // statement.innerHTML("<h4 style='color:blue;'> You're straight! Congrats! Your people are the reason that the majority of the human population are here! Your code is (+-). </h4>")
-        statement.insertAdjacentHTML("afterend", straight);
+        statement.insertAdjacentHTML("afterbegin", straight);
     }
     else if ((iAM === iMasc && imINTO === theyMasc) || (iAM === iFem && imINTO === theyFem)) {
         //statement.innerHTML("<h4 style='color:purple;'> You're GAY BESTIE! Congratulations!  Step 1:Eyebrows MATTER. They shape the face and will change your whole look. Please keep them clean friend. Growing up will be hard but it gets better. You code is (++) or (--). </h4>")
-        statement.insertAdjacentHTML("afterend", gay);
+        statement.insertAdjacentHTML("afterbegin", gay);
     }
     else if ((iAM === iMasc && imINTO === theyMascFem) || (iAM === iFem && imINTO === theyMascFem) || (iAM === iMascFem && imINTO === theyMascFem)) {
         // statement.innerHTML("<h4 style='color:yellow;'> You're bisexual or a variation of bisexual! You get to participate in the best of both worlds! Lucky you! C: Your code is x(+-). </h4>")
-        statement.insertAdjacentHTML("afterend", bisexual);
+        statement.insertAdjacentHTML("afterbegin", bisexual);
     }
     else if ((iAM === iFem || iMasc || iMascFem) && (imINTO === nothing)) {
         // statement.innerHTML("<h4 style='color:white;'> Welcome to the A-TEAM! You're asexual & thats awesome! Your code is x(,)</h4>")
-        statement.insertAdjacentHTML("afterend", asexual);
+        statement.insertAdjacentHTML("afterbegin", asexual);
     }
     else {
         //  statement.innerHTML("<h4 style='color:red;'> Hey friend..You've gotta make a choice by selecting what energy you give off & the energy you're into before clicking submit. Try again!.. or dont. C: </h4>")
-        statement.insertAdjacentHTML("afterend", noans);
+        statement.insertAdjacentHTML("afterbegin", noans);
     }
 }
