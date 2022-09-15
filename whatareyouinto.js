f4140aaf1fdeeafdddaa7d1440f5596745837083
johneyaugusteami <jrmartinez1@my.waketech.edu>
2022-09-15 00:32:25 -0400

    still working on it when i shoudl be studying

--- whatareyouinto.js
+++ whatareyouinto.js
@@ -2,7 +2,7 @@
 function submitAnswers() {
     let theyMasc = document.getElementById("theyMasc"),
         theyFem = document.getElementById("theyFem"),
-        theyMasFem = document.getElementById("theyMascFem"),
+        theyMascFem = document.getElementById("theyMascFem"),
         iMasc = document.getElementById("iMasc"),
         iFem = document.getElementById("iFem"),
         iMascFem = document.getElementById("iMascFem"),
@@ -13,9 +13,10 @@ function submitAnswers() {
     if ((iAM === iMasc && imINTO === theyFem) || (iAM === iFem && imINTO === theyMasc)) {
         statement.innerHTML("<h4 style='color:blue;'>You're straight! Your people are the reason that 100% of the population are here!</h4>")
     }
-    else if ((iAM === iMasc && imINTO === theyMasc) || (iAM === iFem && imINTO === theyFem)) {
+    else if ((iAM === iMasc && imINTO === theyMasc) || (iAM === iFem && imINTO === theyFem) || (iAM === iMascFem && imINTO === theyMasc || theyFem)) {
         statement.innerHTML("<h4 style='color:purple;'>You're GAY BESTIE! Congratulations, because you're automatically just a funner person. Growing up will be hard but it gets better.</h4>")
     }
+    else if ((iAM === iMasc && imINTO === theyMascFem) || (iAM === iFem && imINTO === theyMascFem) || (iAM === iMascFem && imINTO === theyMascFem)) { }
     else {
         statement.innerHTML("<h4 style='color:red;'>Hey friend..You've gotta make a choice before clicking submit. Try again!.. or dont. C:</h4>")
     }
