84aa6e609a94d22eede7c3f8b1660815114f02c2
johneyaugusteami <jrmartinez1@my.waketech.edu>
2022-09-15 00:14:56 -0400

    got a lot done~ but not close to done..

--- whatareyouinto.js
+++ whatareyouinto.js
@@ -1,7 +1,24 @@
-let uMasc = document.getElementById("iMasc"),
-    uFem = document.getElementById("uFem"),
-    uMasFem = document.getElementById("uMascFem"),
-    iMasc = document.getElementById("iMasc"),
-    iFem = document.getElementById("iFem"),
-    iMascFem = document.getElementById("iMascFem");
 
+function submitAnswers() {
+    let theyMasc = document.getElementById("theyMasc"),
+        theyFem = document.getElementById("theyFem"),
+        theyMasFem = document.getElementById("theyMascFem"),
+        iMasc = document.getElementById("iMasc"),
+        iFem = document.getElementById("iFem"),
+        iMascFem = document.getElementById("iMascFem"),
+        iAM = document.getElementById("iAM"),
+        imINTO = document.getElementById("imINTO"),
+        statement = document.getElementById("statement");
+
+    if ((iAM === iMasc && imINTO === theyFem) || (iAM === iFem && imINTO === theyMasc)) {
+        statement.innerHTML("<h4 style='color:blue;'>You're straight! Your people are the reason that 100% of the population are here!</h4>")
+    }
+    else if ((iAM === iMasc && imINTO === theyMasc) || (iAM === iFem && imINTO === theyFem)) {
+        statement.innerHTML("<h4 style='color:purple;'>You're GAY BESTIE! Congratulations, because you're automatically just a funner person. Growing up will be hard but it gets better.</h4>")
+    }
+    else {
+        statement.innerHTML("<h4 style='color:red;'>Hey friend..You've gotta make a choice before clicking submit. Try again!.. or dont. C:</h4>")
+    }
+}
+
+statement.onclick = submitAnswers()
