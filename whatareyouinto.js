d265bd85b2163fe0dd6f0b901707c87bbed1f6d2
johneyaugusteami <jrmartinez1@my.waketech.edu>
2022-09-14 23:37:27 -0400

    update// started on js

---
+++ whatareyouinto.js
@@ -0,0 +1,7 @@
+let uMasc = document.getElementById("iMasc"),
+    uFem = document.getElementById("uFem"),
+    uMasFem = document.getElementById("uMascFem"),
+    iMasc = document.getElementById("iMasc"),
+    iFem = document.getElementById("iFem"),
+    iMascFem = document.getElementById("iMascFem");
+
