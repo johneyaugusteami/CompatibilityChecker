f260525bc5c4ef627f08dba9787f0ca8185f75fc
johneyaugusteami <jrmartinez1@my.waketech.edu>
2022-09-16 19:22:45 -0400

    AHMED HELPED ME FIX IT

--- whatareyouinto.js
+++ whatareyouinto.js
@@ -1,14 +1,9 @@
 
 function submitAnswers() {
-    let theyMasc = document.getElementById("theyMasc"),
-        theyFem = document.getElementById("theyFem"),
-        theyMascFem = document.getElementById("theyMascFem"),
-        iMasc = document.getElementById("iMasc"),
-        iFem = document.getElementById("iFem"),
-        iMascFem = document.getElementById("iMascFem"),
+
+    let
         iAM = document.getElementById("iAM"),
         imINTO = document.getElementById("imINTO"),
-        nothing = document.getElementById("nothing"),
         statement = document.getElementById("statement"),
         gay = "<h4 style='color:purple;'> You're GAY/LESBIAN BESTIE! Congratulations! Keep your head held high King/Queen! Growing up will be hard but it gets better.</h4>",
         bisexual = "<h4 style='color:yellow;'> You're bisexual or a variation of bisexual! You get to participate in the best of both worlds! Congrats! C: </h4>",
@@ -16,25 +11,38 @@ function submitAnswers() {
         noans = "<h4 style='color:red;'> Hey friend..You've gotta make a choice by selecting what energy you give off & the energy you're into before clicking submit. Try again!.. or dont. C: </h4>",
         straight = "<h4 style='color:blue;'> You're Straight! Congrats! You and other straight baby making people are the reason that the majority of the human population are here! V gratetful for you! </h4>";
 
+    statement.innerHTML = noans;
 
-    if ((iAM === iMasc && imINTO === theyFem) || (iAM === iFem && imINTO === theyMasc)) {
-        // statement.innerHTML("<h4 style='color:blue;'> You're straight! Congrats! Your people are the reason that the majority of the human population are here! Your code is (+-). </h4>")
-        statement.insertAdjacentHTML("afterbegin", straight);
+    if (iAM.value == "Masculine") {
+        if (imINTO.value == "Masculine") {
+            statement.innerHTML = gay;
+        }
+        if (imINTO.value == "Feminine") {
+            statement.innerHTML = straight;
+        }
     }
-    else if ((iAM === iMasc && imINTO === theyMasc) || (iAM === iFem && imINTO === theyFem)) {
-        //statement.innerHTML("<h4 style='color:purple;'> You're GAY BESTIE! Congratulations!  Step 1:Eyebrows MATTER. They shape the face and will change your whole look. Please keep them clean friend. Growing up will be hard but it gets better. You code is (++) or (--). </h4>")
-        statement.insertAdjacentHTML("afterbegin", gay);
+
+    if (iAM.value == "Feminine") {
+        if (imINTO.value == "Masculine") {
+            statement.innerHTML = straight;
+        }
+        if (imINTO.value == "Feminine") {
+            statement.innerHTML = gay;
+        }
     }
-    else if ((iAM === iMasc && imINTO === theyMascFem) || (iAM === iFem && imINTO === theyMascFem) || (iAM === iMascFem && imINTO === theyMascFem)) {
-        // statement.innerHTML("<h4 style='color:yellow;'> You're bisexual or a variation of bisexual! You get to participate in the best of both worlds! Lucky you! C: Your code is x(+-). </h4>")
-        statement.insertAdjacentHTML("afterbegin", bisexual);
+
+    if (iAM.value == "Both") {
+        if (imINTO.value == "Masculine") {
+            statement.innerHTML = gay;
+        }
+        if (imINTO.value == "Feminine") {
+            statement.innerHTML = straight;
+        }
     }
-    else if ((iAM === iFem || iMasc || iMascFem) && (imINTO === nothing)) {
-        // statement.innerHTML("<h4 style='color:white;'> Welcome to the A-TEAM! You're asexual & thats awesome! Your code is x(,)</h4>")
-        statement.insertAdjacentHTML("afterbegin", asexual);
+
+    if (imINTO.value == "Nothing") {
+        statement.innerHTML = asexual;
     }
-    else {
-        //  statement.innerHTML("<h4 style='color:red;'> Hey friend..You've gotta make a choice by selecting what energy you give off & the energy you're into before clicking submit. Try again!.. or dont. C: </h4>")
-        statement.insertAdjacentHTML("afterbegin", noans);
-    }
+
 }
+
