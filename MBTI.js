1f46de6a674d24cd1c2bb275c54f1a7ae4c991a0
johneyaugusteami <jrmartinez1@my.waketech.edu>
2022-09-19 03:50:32 -0400

    commit

--- MBTI.js
+++ MBTI.js
@@ -5,6 +5,15 @@ function submitAnswers() {
         iAM = document.getElementById("iAM"),
         theyARE = document.getElementById("theyARE"),
         statement = document.getElementById("statement"),
+        ISTPxISTP = "<h4 style='color:yellow;'>This Pair isnt ideal..But it can work!</h4> <br/> <h4 style='color:#e2857a;'>One of the most valuable aspects of the ISTJ-ISTJ relationship is the ability to share and compare notes about human experiences. They may be too similar in the wrong way because they share the same weaknesses. As a result, instead of focusing on growth, they tend to reinforce each other's vulnerabilities and rampant behaviors.</h4> <br/>",
+
+
+
+
+
+
+
+
         noans = "<h4 style='color:red;'> Hey friend..You've gotta make a choice by selecting your personality type & the person in questions personality type.</h4>";
 
 
@@ -20,52 +29,52 @@ function submitAnswers() {
     //ISTP
     if (iAM.value == "ISTP") {
         if (theyARE.value == "ISTP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxISTP;
         }
         if (theyARE.value == "ISFP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxISFP;
         }
         if (theyARE.value == "ESTP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxESTP;
         }
         if (theyARE.value == "ESFP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxESFP;
         }
         if (theyARE.value == "INTJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxINTJ;
         }
         if (theyARE.value == "INTP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxINTP;
         }
         if (theyARE.value == "ENTJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxENTJ;
         }
         if (theyARE.value == "ENTP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxENTP;
         }
         if (theyARE.value == "INFJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxINFJ;
         }
         if (theyARE.value == "INFP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxINFP;
         }
         if (theyARE.value == "ENFJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxENFJ;
         }
         if (theyARE.value == "ENFP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxENFP;
         }
         if (theyARE.value == "ISTJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxISTJ;
         }
         if (theyARE.value == "ISFJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxISFJ;
         }
         if (theyARE.value == "ESTJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxESTJ;
         }
         if (theyARE.value == "ESFJ") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxESFJ;
         }
     }
 
@@ -76,13 +85,13 @@ function submitAnswers() {
     //ISFP
     if (iAM.value == "ISFP") {
         if (theyARE.value == "ISTP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISTPxISFP;
         }
         if (theyARE.value == "ISFP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISFPxISFP;
         }
         if (theyARE.value == "ESTP") {
-            statement.innerHTML = variableforcouple;
+            statement.innerHTML = ISFPxESTP;
         }
         if (theyARE.value == "ESFP") {
             statement.innerHTML = variableforcouple;
